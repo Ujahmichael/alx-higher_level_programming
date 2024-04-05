8a47906175edeaa3928a4ea3f00050deea842ff2
ujahmichael <hrmmajestyuja@gmail.com>
2024-04-05 01:27:05 -0700

    t2-4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,16 +1,19 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
-"""THIS PYTHON CODE IS WRITTEN BY UJAH MICHAEL(MAJESTI)"""
+"""Define a class Square."""
 
-"""Define a clase square."""
 
 class Square:
-    """initializing the squiare value
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """Initialize a new square.
+
         Args:
             size (int): The size of the new square.
         """
-    def __init__(self, size=0):
         self.size = size
+
     @property
     def size(self):
         """Get/set the current size of the square."""
@@ -18,11 +21,13 @@ class Square:
 
     @size.setter
     def size(self, value):
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
+
     def area(self):
-        """Return area of square"""
-        return (self.__size ** 2)
+        """Return the current area of square."""
+        return (self.__size * self.__size)
