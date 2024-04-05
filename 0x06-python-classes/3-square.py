4b40397cf6868d4524c100f4f5be6ee411b0b733
ujahmichael <hrmmajestyuja@gmail.com>
2024-04-05 01:29:52 -0700

    t2-4

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python
 
-"""THIS PYTHON CODE IS WRITTEN BY UJAH MICHAEL(MAJESTI)"""
-
 """Define a clase square."""
 
 class Square:
+
     """initializing the squiare value
         Args:
             size (int): The size of the new square.
         """
+
     def __init__(self, size=0):
         self.__size = size
 
@@ -16,6 +16,7 @@ class Square:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
+
     def area(self):
         """Return area of square"""
         return (self.__size ** 2)
