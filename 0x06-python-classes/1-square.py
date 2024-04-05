2a08447eca51b5d6e3cf9009ff6690a969f7000a
ujahmichael <hrmmajestyuja@gmail.com>
2024-04-04 23:39:06 -0700

    t1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,10 +3,14 @@
 """Define a class Square."""
 
 class square:
+
     """this is a square"""
 
     def __init__(self, size):
 
-        """ Initializing a square."""
+        """ Initializing a square.
+        Args:
+            size (int): The size of the new square.
+        """
 
         self.__size = size
