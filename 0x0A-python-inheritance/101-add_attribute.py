390a5fcceb7fce8b5b41db5ddb9cbc41fcc3a05f
ujahmichael <hrmmajestyuja@gmail.com>
2024-04-15 17:09:01 -0700

    checks on failing tasks

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
-""" funcion that adds a new attribute when possible """
+"""
+Module 101-add_attribute
 
-def add_attribute(obj, attr, value):
-    """ adds new attribute """
-    obj.attr = value
+Contains function that adds new attribute if possible
+"""
+
+
+def add_attribute(obj, attribute, value):
+    """
+    add attribute to object if possible
+    """
+    if ('__dict__' in dir(obj)):
+        setattr(obj, attribute, value)
+    else:
+        raise TypeError("can't add new attribute")
